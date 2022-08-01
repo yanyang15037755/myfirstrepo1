6b6083dd1674fc8e5b3641c1e098ca76fadd102f
yanyang15037755 <108185733+yanyang15037755@users.noreply.github.com>
2022-08-01 13:32:22 +0800

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector('.toggle')
+const showcase = document.querySelector('.showcase')
+
+toggle.addEventListener('click',()=> {
+   toggle.classList.toggle('active')
+   showcase.classList.toggle('active')
+})
